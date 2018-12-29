420bbaee66e4a67537037b3cec6fc51be3a46db0
sp1npx <spinpx@gmail.com>
2018-12-29 20:23:37 +0800

    fix issue#11

--- build/docker_build.sh
+++ build/docker_build.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 apt-get update
 apt-get install -y git build-essential wget zlib1g-dev golang-go python-pip python-dev build-essential 
 
-PREFIX=/ sh ./build/llvm.sh
+PREFIX=/ ./build/llvm.sh
 
 # from https://github.com/rust-lang-nursery/docker-rust-nightly/blob/master/nightly/Dockerfile
 url="https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
@@ -19,7 +19,7 @@ rustup --version
 cargo --version
 rustc --version
 
-sh ./build/build.sh
+./build/build.sh
 
 #wllvm and gllvm
 pip install --upgrade pip==9.0.3
