8ab96f4e6d739899eb703a8f8be462ed56545312
maverick117 <j.zhong0@gmail.com>
2019-01-20 17:05:54 +0800

    Revert build script

--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-setup_llvm() {
-    ./build/llvm.sh
-    export PATH=${HOME}/clang+llvm/bin:$PATH
-    export LD_LIBRARY_PATH=${HOME}/clang+llvm/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-}
-
 set -euxo pipefail
 
 if ! [ -x "$(command -v llvm-config)"  ]; then
-    setup_llvm
-elif ! [ $(llvm-config --version) == "7.0.1" ]; then
-    setup_llvm
+    ./build/llvm.sh
+    export PATH=${HOME}/clang+llvm/bin:$PATH
+    export LD_LIBRARY_PATH=${HOME}/clang+llvm/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 fi
 
 cargo build
