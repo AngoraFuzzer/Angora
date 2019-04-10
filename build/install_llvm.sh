29c6392ae3002ac53d0696089b8f960fa029d5e9
Kasper <kfoens@gmail.com>
2019-04-10 16:39:31 +0200

    Use a https url for fetching llvm

--- build/install_llvm.sh
+++ build/install_llvm.sh
@@ -5,7 +5,7 @@ LINUX_VER=${LINUX_VER:-ubuntu-16.04}
 LLVM_VER=${LLVM_VER:-4.0.0}
 PREFIX=${PREFIX:-${HOME}}
 
-LLVM_DEP_URL=http://releases.llvm.org/${LLVM_VER}
+LLVM_DEP_URL=https://releases.llvm.org/${LLVM_VER}
 TAR_NAME=clang+llvm-${LLVM_VER}-x86_64-linux-gnu-${LINUX_VER}
 
 wget -q ${LLVM_DEP_URL}/${TAR_NAME}.tar.xz
