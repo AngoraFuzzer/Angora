0ccef63b0d057b6739537e427e67e7ae745dbf51
sp1npx <spinpx@gmail.com>
2019-06-14 12:39:43 +0800

    fix cxx11

--- build/build.sh
+++ build/build.sh
@@ -12,22 +12,22 @@ if ! [ -x "$(command -v llvm-config)"  ]; then
     export CXX=clang++
 fi
 
-PREFIX=${PREFIX:-${ROOT_DIR}}
+PREFIX=${PREFIX:-${ROOT_DIR}/bin/}
 
 cargo build
 cargo build --release
 
-rm -rf ${PREFIX}/bin
-mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/bin/lib
-cp target/release/fuzzer ${PREFIX}/bin
-cp target/release/*.a ${PREFIX}/bin/lib
+rm -rf ${PREFIX}
+mkdir -p ${PREFIX}
+mkdir -p ${PREFIX}/lib
+cp target/release/fuzzer ${PREFIX}
+cp target/release/*.a ${PREFIX}/lib
 
 cd llvm_mode
 rm -rf build
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX}/bin/ -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
 make # VERBOSE=1 
 make install # VERBOSE=1
 
